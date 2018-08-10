ea76e149d1b176fa6f6bc0aa3550f98b697826ba
lth <lth@litonghui>
2018-08-10 16:49:49 +0800

    集成录屏功能

---
+++ src/main/java/cn/util/CapUtils.java
@@ -0,0 +1,15 @@
+package cn.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CapUtils {
+
+	  public static String URL = "/";
+	    
+	   public static List getCaptureFolder(){
+	    	List list = new ArrayList();
+	    	return list;
+	    }
+
+}
